7f126b8c5a0c6701b54b4b7f91a9a5792b3123e6
BrunoReis25 <pessoadosreisbruno@gmail.com>
2022-01-25 11:00:49 -0300

    aula02

--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,9 @@
 import { Box, Button, Text, TextField, Image } from '@skynexui/components';
+import React from 'react';
 import appConfig from '../config.json';
+import {useRouter} from 'next/router'
 
-function GlobalStyle() {
-      return (
-            <style global jsx>{`
-                  * {
-                        margin: 0;
-                        padding: 0;
-                        box-sizing: border-box;
-                        list-style: none;
-                  }
-                  body {
-                        font-family: 'Open Sans', sans-serif;
-                  }
-                  /* App fit Height */ 
-                  html, body, #__next {
-                        min-height: 100vh;
-                        display: flex;
-                        flex: 1;
-                  }
-                  #__next {
-                        flex: 1;
-                  }
-                  #__next > * {
-                        flex: 1;
-                  }
-                  /* ./App fit Height */
-            `}</style>
-      )
-}
+
 
 function Titulo(props) {
       const Tag = props.tag || 'h1';
@@ -37,9 +12,9 @@ function Titulo(props) {
                   <Tag>{props.children}</Tag>
                   <style jsx>{`
                         ${Tag} {
-                        color: ${appConfig.theme.colors.neutrals['000']};
-                        font-size: 25px;
-                        font-weight: 600;
+                              color: ${appConfig.theme.colors.neutrals['000']};
+                              font-size: 25px;
+                              font-weight: 600;
                         }
                   `}</style>
             </>
@@ -62,11 +37,13 @@ function Titulo(props) {
 // export default HomePage
 
 export default function PaginaInicial() {
-      const username = 'peas';
+      // const username = 'BrunoReis25';
+      const [username, setUsername]= React.useState('');
+      const roteamento = useRouter();
+
 
       return (
             <>
-                  <GlobalStyle />
                   <Box
                         styleSheet={{
                               display: 'flex', alignItems: 'center', justifyContent: 'center',
@@ -93,6 +70,12 @@ export default function PaginaInicial() {
                               {/* Formulário */}
                               <Box
                                     as="form"
+                                    onSubmit={function (infosDoEvento){
+                                          infosDoEvento.preventDefault();
+                                          console.log('alguém submeteu uma página.');
+                                          // window.location.href = '/chat';
+                                          roteamento.push('/chat');
+                                    }}
                                     styleSheet={{
                                           display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
                                           width: { xs: '100%', sm: '50%' }, textAlign: 'center', marginBottom: '32px',
@@ -103,8 +86,24 @@ export default function PaginaInicial() {
                                           {appConfig.name}
                                     </Text>
 
+                                    {/* <input
+                                          type="text"
+                                          value={username}
+                                          onChange={function (event) {
+                                                console.log('usuario digitou', event.target.value);
+                                                const valor = event.target.value;
+
+                                                setUsername(valor);
+                                          }}
+                                    /> */}
                                     <TextField
                                           fullWidth
+                                          onChange={function (event) {
+                                                console.log('usuario digitou', event.target.value);
+                                                const valor = event.target.value;
+
+                                                setUsername(valor);
+                                          }}
                                           textFieldColors={{
                                                 neutral: {
                                                       textColor: appConfig.theme.colors.neutrals[200],
